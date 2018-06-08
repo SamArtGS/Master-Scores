ccaf7d8cf7b8d365384e35a3d0c6d4e452247bb2
Samuel Arturo Garrido Sánchez <samuelarturo@comunidad.unam.mx>
2018-06-08 18:23:11 -0500

    Initial Commit

---
+++ Master Scores/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Master Scores
+//
+//  Created by Samuel Arturo Garrido Sánchez on 08/06/18.
+//  Copyright © 2018 Master Scores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
